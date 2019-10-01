b900fa78b787fe2125545aae9a80ce8923c89d5e
efigue <erick.figueroa@treetop.com>
2019-10-01 13:24:40 -0700

    add workspace and security config changes

--- src/main/java/api/security/WebSecurityConfig.java
+++ src/main/java/api/security/WebSecurityConfig.java
@@ -1,42 +1,17 @@
 package api.security;
 
-import api.security.jwt.AuthenticatedUser;
 import api.security.jwt.JWTAuthenticationFilter;
 import api.security.jwt.JWTLoginFilter;
-import org.springframework.beans.factory.BeanInitializationException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.ldap.core.support.BaseLdapPathContextSource;
 import org.springframework.ldap.core.support.LdapContextSource;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
-import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
-import org.springframework.security.ldap.authentication.BindAuthenticator;
-import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
-import org.springframework.security.ldap.authentication.LdapAuthenticator;
-import org.springframework.security.ldap.ppolicy.PasswordPolicyAwareContextSource;
-import org.springframework.security.ldap.search.FilterBasedLdapUserSearch;
-import org.springframework.security.ldap.search.LdapUserSearch;
-import org.springframework.security.ldap.userdetails.DefaultLdapAuthoritiesPopulator;
-import org.springframework.security.ldap.userdetails.LdapAuthoritiesPopulator;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import javax.annotation.PostConstruct;
-import javax.naming.ldap.LdapContext;
-import java.util.Arrays;
-import java.util.Collection;
 
 @Configuration
 @EnableWebSecurity
@@ -61,10 +36,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .ldapAuthentication()
-                .userSearchFilter("(sAMAccountName={0})")
-                .contextSource(contextSource());
+        auth.ldapAuthentication()
+            .userSearchFilter("(sAMAccountName={0})")
+            .contextSource(contextSource());
     }
 
     @Bean
@@ -78,4 +52,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         bean.afterPropertiesSet();
         return bean;
     }
+
 }
